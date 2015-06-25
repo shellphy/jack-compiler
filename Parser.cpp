1eeb7b5970d7dfac28766709f77cbf6d07d7ecbb
aixiangfei <xiangfeiai.ch@gmail.com>
2015-06-25 11:16:25 +0800

    update

--- Parser.cpp
+++ Parser.cpp
@@ -98,6 +98,8 @@ void Parser::parse()
 {
 	scanner.scanToken();
 	tokens = scanner.getTokens();
+	if (tokens->size() == 1)
+		exit(0);
 	syntaxTree = parse_program();
 	cout << "**************************syntaxTree*****************************" << endl;
 }
@@ -124,7 +126,11 @@ void Parser::readNextToken()
 		tokens->pop_front();
 	}
 	else
+	{
+		syntaxError(currentToken.lexeme);
 		exit(-1);
+	}
+		
 }
 
 void Parser::match(string expectedString)
@@ -207,7 +213,10 @@ Parser::TreeNode * Parser::parse_declaration()
 		tokens->pop_front();
 	}
 	else
+	{
+		syntaxError(currentToken.lexeme);
 		exit(-1);
+	}
 	
 	if (!tokens->empty())
 	{
@@ -215,7 +224,11 @@ Parser::TreeNode * Parser::parse_declaration()
 		tokens->pop_front();
 	}
 	else
+	{
+		syntaxError(currentToken.lexeme);
 		exit(-1);
+	}
+		
 
 	if (thirdToken.kind == Scanner::LPARAN)
 	{	
